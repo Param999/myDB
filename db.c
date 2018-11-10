f5f3b7a290f8c42d24160046d132c0e84501f2d0
Param999 <pgpro111@gmail.com>
2018-11-10 19:57:44 +0530

    intial commit - REPL

---
+++ db.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+typedef struct InputBuffer_t{
+	char *buffer;
+	size_t buffer_length;
+	ssize_t input_length;
+} InputBuffer;
+
+InputBuffer* new_input_buffer(){
+	InputBuffer* input_buffer = malloc(sizeof(InputBuffer));
+	input_buffer->buffer = NULL;
+	input_buffer->buffer_length = 0;
+	input_buffer->input_length = 0;
+
+	return input_buffer;
+}
+
+void print_prompt(){
+	printf("db> ");
+}
+
+
+void read_input(InputBuffer* input_buffer){
+	ssize_t bytes_read = getline(&(input_buffer->buffer),&(input_buffer->buffer_length),stdin);
+	
+	if(bytes_read <= 0){
+		printf("Error reading input\n");
+		exit(EXIT_FAILURE);
+	}
+
+	//printf("Buffer length = %zd\n",input_buffer->buffer_length);
+	input_buffer->input_length = bytes_read - 1;
+	input_buffer->buffer[bytes_read - 1] = 0;
+}
+
+int main(){
+	InputBuffer* input_buffer = new_input_buffer();
+	while(1){
+		print_prompt();
+		read_input(input_buffer);
+
+		if(strcmp(input_buffer->buffer, ".exit")==0){
+			exit(EXIT_SUCCESS);
+		} else {
+			printf("Unrecognized command '%s'. \n",input_buffer->buffer);
+		}
+	}	
+}
